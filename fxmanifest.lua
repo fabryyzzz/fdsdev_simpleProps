bcd4980be0e56017d20ead638a7bc7a87380044b
Fabrizio - FDS Development <58892804+fabryyzzz@users.noreply.github.com>
2024-01-24 23:15:12 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'adamant'
+
+-- https://discord.gg/amEJNfn9HX
+-- https://discord.gg/amEJNfn9HX
+-- https://discord.gg/amEJNfn9HX
+-- https://discord.gg/amEJNfn9HX
+-- https://discord.gg/amEJNfn9HX
+-- For other resources consider to Join My discord!
+
+
+-- https://fdsdevelopment.tebex.io
+
+game 'gta5'
+lua54 'yes'
+
+description '[FDSDEV] Simple Props'
+
+version '7.0'
+
+
+client_scripts {
+    'config.lua',
+    'client/client.lua',
+}
